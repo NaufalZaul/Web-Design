061ede01edcf32afb42c87428d34218bcbbf2aa0
Naufal Zaul <77571730+NaufalZaul@users.noreply.github.com>
2022-03-29 11:13:05 +0700

    initial commit

---
+++ button.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.navbar-burger');
+const menu = document.querySelector('.navbar-menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+})
